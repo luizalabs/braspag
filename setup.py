b0f3914b9787dbd75d610aa88cab4c9ab31abd82
StuntS <dnl.stunts@gmail.com>
2014-06-27 14:49:26 -0300

    Updating version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ readme = open(os.path.join(cwd, 'README.md')).read()
 
 setup(
     name='braspag',
-    version='0.4.5',
+    version='0.4.6',
     description = "Python library to consume Braspag SOAP Web services",
     long_description = readme,
     author='Sergio Oliveira',
