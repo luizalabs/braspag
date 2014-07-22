32c193626ba43d6722750ed9008fe8a0a2dc64bb
StuntS <dnl.stunts@gmail.com>
2014-07-22 16:06:46 -0300

    Validating credit_card_token authorization

--- tests/test_authorize.py
+++ tests/test_authorize.py
@@ -60,6 +60,61 @@ class AuthorizeTest(BraspagTestCase):
         assert response.transactions[1]['masked_credit_card_number'] == u'9000********0001'
 
     @gen_test(timeout=ASYNC_TIMEOUT)
+    def test_authorize_with_card_token(self):
+        response = yield self.braspag.authorize(**{
+                                     'request_id': '782a56e2-2dae-11e2-b3ee-080027d29772',
+                                     'order_id': '2cf84e51-c45b-45d9-9f64-554a6e088668',
+                                     'customer_id': '12345678900',
+                                     'customer_name': u'José da Silva',
+                                     'customer_email': 'jose123@dasilva.com.br',
+                                     'transactions': [{
+                                         'amount': 100000,
+                                         'card_holder': 'Jose da Silva',
+                                         'card_number': '0000000000000001',
+                                         'card_security_code': '123',
+                                         'card_exp_date': '05/2018',
+                                         'save_card': True,
+                                         'payment_method': PAYMENT_METHODS['Simulated']['BRL'],
+                                         'soft_descriptor': u'Sax Alto Chinês',
+                                     },{
+                                         'amount': 190099,
+                                         'card_holder': u'João Silveira',
+                                         'card_number': '9000000000000001',
+                                         'card_security_code': '432',
+                                         'card_exp_date': '05/2020',
+                                         'save_card': False,
+                                         'payment_method': PAYMENT_METHODS['Simulated']['BRL'],
+                                         'soft_descriptor': u'Sax Alto Thailandês',
+                                     }],
+                                 })
+        assert response.success == True
+        
+        card_token = response.transactions[0]['card_token']
+        response = yield self.braspag.authorize(**{
+                                     'request_id': '782a56e2-2dae-11e2-b3ee-080027d29772',
+                                     'order_id': '2cf84e51-c45b-45d9-9f64-554a6e088668',
+                                     'customer_id': '12345678900',
+                                     'customer_name': u'José da Silva',
+                                     'customer_email': 'jose123@dasilva.com.br',
+                                     'transactions': [{
+                                         'amount': 100000,
+                                         'card_token': card_token,
+                                         'soft_descriptor': u'Sax Alto Chinês',
+                                         'card_holder': '',
+                                         'card_number': '',
+                                         'card_security_code': '',
+                                         'card_exp_date': '',
+                                         'payment_method': PAYMENT_METHODS['Simulated']['BRL'],
+                                     }],
+                                 })
+        
+        assert response.success == True
+        assert response.transactions[0]['return_code'] == '4'
+        assert response.transactions[0]['return_message'] == u'Operation Successful'
+        assert response.transactions[0]['status'] == 1
+        assert response.transactions[0]['status_message'] == 'Authorized'
+
+    @gen_test(timeout=ASYNC_TIMEOUT)
     def test_wrong_number_of_payments(self):
         with self.assertRaises(BraspagException):
             response = yield self.braspag.authorize(**{
