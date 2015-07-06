3a7e10dc0b1334a741639473528bd4d482ac5379
Daniel Urbano <daniel.urbano@luizalabs.com>
2015-07-06 15:19:34 -0300

    Tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 from braspag.utils import to_bool
 from braspag.utils import to_int
 from braspag.utils import mask_credit_card_from_xml
+from braspag.utils import is_valid_guid
 from .base import BraspagTestCase
 
 
@@ -38,3 +39,12 @@ class UtilsTest(BraspagTestCase):
 
         assert (mask_credit_card_from_xml(xml) ==
                 '<html><head></head><CardNumber>123456******3456</CardNumber><body></body></html>')
+
+    def test_is_valid_guid(self):
+        self.assertTrue(is_valid_guid('555d97f7-92ab-4907-a8d0-f2ba51afe470'))
+        self.assertTrue(is_valid_guid('937f36f1-8b8d-427e-83f1-02faadcdf6eb'))
+        self.assertTrue(is_valid_guid('e3e8699c-7b4d-48f3-bd40-b75eddef0ab5'))
+        self.assertTrue(is_valid_guid('a220b799-2734-4760-9a17-2d1f9f77328b'))
+        self.assertTrue(is_valid_guid('514b9516-0aad-4fef-a1ad-9894cc9c925e'))
+        self.assertTrue(is_valid_guid('92209184-7fd6-4272-b1dc-d60f5590972d'))
+        self.assertTrue(is_valid_guid('a2f27876-c52e-4062-9a37-bc513239af60'))
