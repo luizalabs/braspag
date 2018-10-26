1a61e29579095b36a967467c44078bc54a48fc9b
Rafael Gottsfritz <rafaelgotts@gmail.com>
2018-10-25 23:19:25 -0300

    Use vcrpy on all test_authorize like

--- tests/test_authorize_and_void.py
+++ tests/test_authorize_and_void.py
@@ -1,33 +1,36 @@
 # -*- coding: utf8 -*-
-
 from __future__ import absolute_import
-
 import logging
-from braspag.consts import PAYMENT_METHODS
-from .base import BraspagTestCase
+
 from tornado.testing import gen_test
+
+from braspag.consts import PAYMENT_METHODS
+
+from .base import BraspagTestCase
+from .vcrutils import replay
 
 
 class AuthorizeAndVoidTest(BraspagTestCase):
 
     @gen_test
+    @replay
     def test_authorize_and_void(self):
         response = yield self.braspag.authorize(**{
-                                     'request_id': '782a56e2-2dae-11e2-b3ee-080027d29772',
-                                     'order_id': '2cf84e51-c45b-45d9-9f64-554a6e088668',
-                                     'customer_id': '12345678900',
-                                     'customer_name': u'José da Silva',
-                                     'customer_email': 'jose123@dasilva.com.br',
-                                     'transactions': [{
-                                         'amount': 100000,
-                                         'card_holder': 'Jose da Silva',
-                                         'card_number': '0000000000000001',
-                                         'card_security_code': '123',
-                                         'card_exp_date': '05/2018',
-                                         'save_card': True,
-                                         'payment_method': PAYMENT_METHODS['Simulated']['BRL'],
-                                     }],
-                                 })
+            'request_id': '782a56e2-2dae-11e2-b3ee-080027d29772',
+            'order_id': '2cf84e51-c45b-45d9-9f64-554a6e088668',
+            'customer_id': '12345678900',
+            'customer_name': u'José da Silva',
+            'customer_email': 'jose123@dasilva.com.br',
+            'transactions': [{
+                'amount': 100000,
+                'card_holder': 'Jose da Silva',
+                'card_number': '0000000000000001',
+                'card_security_code': '123',
+                'card_exp_date': '05/2018',
+                'save_card': True,
+                'payment_method': PAYMENT_METHODS['Simulated']['BRL'],
+            }],
+        })
 
         assert response.success == True
         assert response.transactions[0]['amount'] == 100000
@@ -39,10 +42,11 @@ class AuthorizeAndVoidTest(BraspagTestCase):
         assert response.transactions[0]['status'] == 1  # TODO: transformar em constante, tabela 13.10.1 do manual do pagador
         assert response.transactions[0]['status_message'] == 'Authorized'
 
-        response = yield self.braspag.void(transaction_id=response.transactions[0]['braspag_transaction_id'],
-                            amount=response.transactions[0]['amount'],
-                            request_id=response.correlation_id,
-                            )
+        response = yield self.braspag.void(
+            transaction_id=response.transactions[0]['braspag_transaction_id'],
+            amount=response.transactions[0]['amount'],
+            request_id=response.correlation_id,
+        )
 
         assert response.success == True
         assert response.transactions[0]['amount'] == 100000
